192a706fd1cf75ea2f2c44d14e49fb635f8a947f
Nutty Geek <nuttygeek@Nuttys-MacBook-Air.local>
2021-05-30 23:04:15 +0530

    working app

--- certificate/index.js
+++ certificate/index.js
@@ -1,15 +1,38 @@
-import { jsPDF } from "jspdf";
 // Default export is a4 paper, portrait, using millimeters for units
-const printBtn = document.getElementById('print');
-printBtn.addEventListener('click', () => {
-    const doc = new jsPDF({
-        orientation: 'landscape'
-    });
-    var elementHTML = $('#certificate').html();
-    doc.fromHTML(elementHTML, 15, 15, {
-        'width': 180,
-        'elementHandlers': specialElementHandlers
-    });
-    // Save the PDF
-    doc.save('sample-document.pdf');
-});
+// const printBtn = document.getElementById('print');
+// printBtn.addEventListener('click', () => {
+//     window.print();
+// });
+
+window.onload = function() {
+    titleEl = document.getElementById('title');
+    nameEl = document.getElementById('name');
+    mobileEl = document.getElementById('mobile');
+    stateEl = document.getElementById('state');
+    districtEl = document.getElementById('district');
+    pincodeEl = document.getElementById('pincode');
+    emailEl = document.getElementById('email');
+}
+
+function submit() {
+    // let url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdSUTJsZjKa18v-D6AKMPATVaTNvb8Wd5GZZTM3pmDpPz-0BQ/formResponse";
+    let obj = {
+        title: titleEl.value,
+        name: nameEl.value,
+        mobile: mobileEl.value,
+        state: stateEl.value,
+        district: districtEl.value,
+        pincode: pincodeEl.value,
+        email: emailEl.value
+    };
+    saveData(obj);
+    window.location.href = 'certificate.html';
+}
+
+
+function saveData(data) {
+    localStorage.setItem('data', JSON.stringify(data));
+}
+
+
+
